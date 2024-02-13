be656bba3e1886c3db6acf6b7a13186f1d18f2c1
19Phin <19phin@gmail.com>
2024-02-13 16:49:48 -0700

    1.19-1.19.2

--- src/main/java/net/dialingspoon/multicount/mixin/ClientPlayerMixin.java
+++ src/main/java/net/dialingspoon/multicount/mixin/ClientPlayerMixin.java
@@ -5,9 +5,11 @@ import net.dialingspoon.multicount.Multicount;
 import net.dialingspoon.multicount.server.interfaces.PlayerAdditions;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.encryption.PlayerPublicKey;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,8 +21,8 @@ public abstract class ClientPlayerMixin extends PlayerEntity implements PlayerAd
     @Unique
     public int account;
 
-    public ClientPlayerMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
-        super(world, pos, yaw, gameProfile);
+    public ClientPlayerMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile, @Nullable PlayerPublicKey publicKey) {
+        super(world, pos, yaw, gameProfile, publicKey);
     }
 
     @Override
