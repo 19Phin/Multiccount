c77b17c6159f6890abb0845a2ddb57353474fb25
DiallingSpoon527 <123208557+19Phin@users.noreply.github.com>
2025-05-14 15:25:52 -0700

    Fixed a rare error with mod compatibility where it would call accountStates before it was defined.

--- src/main/java/net/dialingspoon/multicount/Multicount.java
+++ src/main/java/net/dialingspoon/multicount/Multicount.java
@@ -2,8 +2,10 @@ package net.dialingspoon.multicount;
 
 import net.dialingspoon.multicount.util.AccountStates;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.datafixer.DataFixTypes;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.PersistentState;
 import net.minecraft.world.World;
 import org.slf4j.Logger;
@@ -17,7 +19,9 @@ public class Multicount implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// Initialize persistent state
-		ServerLifecycleEvents.SERVER_STARTED.register(server ->
+		Identifier priorityPhase = Identifier.of(MOD_ID, "default");
+		ServerLifecycleEvents.SERVER_STARTED.addPhaseOrdering(priorityPhase, Event.DEFAULT_PHASE);
+		ServerLifecycleEvents.SERVER_STARTED.register(priorityPhase, server ->
 				accountStates = server.getWorld(World.OVERWORLD).getPersistentStateManager().getOrCreate(new PersistentState.Type<>(AccountStates::new, AccountStates::fromNbt, DataFixTypes.LEVEL), Multicount.MOD_ID
 				));
 	}
